438e3488bc7b93bca7ba309b18f855f0412c70a7
Levi W <leviwilliamsemail@gmail.com>
2022-03-25 18:22:56 -0600

    T2 Keycard loot

--- KeyCardSystemServerConfig/4_World/SecurityDoorScriptsConfig/Config/global.c
+++ KeyCardSystemServerConfig/4_World/SecurityDoorScriptsConfig/Config/global.c
@@ -48,5 +48,25 @@ const static ref array<string> KEYCARD_ATTACHMENTS_MAGS_T1 =
 const static ref array<string> KEYCARD_OTHER_T1 =
 {
     "NailBox", "NailBox", "CodeLock", "WoodAxe", "BandageDressing", "Epinephrine", "Morphine", "MediumTent",
-    "Hatchet", "BBP_Mortar_Mix", "BBP_Wallpaper", "BBP_Step_LadderKit", "RFFSHeli_flight_case_red"
+    "Hatchet", "BBP_Mortar_Mix", "BBP_Wallpaper", "BBP_Step_LadderKit", "RFFSHeli_flight_case_red", "SalineBagIV"
+};
+
+const static ref array<string> KEYCARD_WEAPONS_T2 =
+{
+    "Mosin9130", "Winchester70", "SVD", "A2FNFAL", "CPB_HK417_Black", "A2mk17", "CPB_M32", "CPB_RPK16_Black"
+};
+
+const static ref array<string> KEYCARD_ATTACHMENTS_MAGS_T2 = 
+{
+    "Mag_SVD_10Rnd", "A2FNFAL_Magazine", "CPB_HK417_20rnd_Magazine", "CPB_ATN_Black", "CPB_ELCAN", "CPB_PEScope",
+    "A2M4SPRScope", "Mosin_Compensator", "A2mk17_mag", "A2Suppressor", "AK_Suppressor", "CPB_RPK16_DrumMagazine",
+    "CPB_Bipod_Black", "AmmoBox_308Win_20Rnd", "CPB_AmmoBox_408Chey", "AmmoBox_762x39_20Rnd", "AmmoBox_762x54_20Rnd "
+};
+
+const static ref array<string> KEYCARD_OTHER_T2 = 
+{
+    "NailBox", "CodeLock", "BandageDressing", "Epinephrine", "Morphine", "CarTent",  "Hatchet",
+    "BBP_Mortar_Mix", "MMG_carrier_backpack_black", "MMG_camelback_black", "MMG_supplybag_black",
+    "MMG_tt_vest_erdl", "MMG_striker_helmet_erdl", "SalineBagIV", "BBP_T2_DoorKit", "BBP_T2_GateKit",
+    "BearPelt", "RedemptionKeyCard_03"
 };
